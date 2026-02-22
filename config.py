0d387ad6a99f68d093c3cc5cf94bc58658413c34
Farid Karimli <karimli.farid@icloud.com>
2026-02-21 19:43:36 -0500

    Add error annotation file to config

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 VIDEO_DIRECTORY = "./captain_cook_4d/gopro/resolution_360p"
 
 STEP_ANNOTATION_JSON = "./captain_cook_4d/gopro/resolution_360p/downloaded_video_annotations.json"
+ERROR_ANNOTATION_JSON = "./downloader/metadata/error_annotations.json"
+
 
 ERROR_RECOGNITION_RESULTS_JSON = "./error_recognition/results/error_recognition_task1_results.json"
